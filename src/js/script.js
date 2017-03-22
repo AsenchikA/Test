22b6ab61e5e1ed6660164ecdd42c3803f1ef8520
Asenchik Anastasiya <n-asenchik@yandex.ru>
2017-03-22 23:23:45 +0400

    Стилизация блока promo

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,14 @@ $(document).ready(function(){
   // бургер в header
   var toggler = document.getElementById('toggler');
   toggler.onclick = function(e){
-  e.preventDefault();
-  toggler.classList.toggle('toggler--close');
-}
+    e.preventDefault();
+    toggler.classList.toggle('toggler--close');
+  };
+
+  $("#promo-slider").owlCarousel({
+    loop: true,
+    items:1,
+    dots:true,
+    dotsEach: true
+  });
 });
