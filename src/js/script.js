e9d4221ca4770472b48ec27cb97fa671304fd098
Asenchik Anastasiya <n-asenchik@yandex.ru>
2017-03-27 17:32:24 +0400

    Подключены необходимые шрифты

--- src/js/script.js
+++ src/js/script.js
@@ -18,4 +18,10 @@ $(document).ready(function(){
     $(this).toggleClass("footer-menu__item--open");
     $(this).children().toggleClass("footer-submenu--visible");
   });
+
+  loadFont(
+  'AvenirNext',                      // название шрифта
+ '/css/font_AvenirNext_woff.css?v=1',   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF
+  '/css/font_AvenirNext_woff2.css?v=1'   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF2
+);
 });
